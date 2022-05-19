7dd98d2cd790067f8b2177ef75cceba6de1d041b
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2022-05-19 17:34:19 +0700

    update  responsive

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const btnOnMenu = document.querySelector(".header-nav-menu");
+const btnCloseMenu = document.querySelector(".mobile-header-close");
+const resMenu = document.querySelector(".header_nav_mobile")
+btnOnMenu.onclick = function(){
+    resMenu.classList.toggle("none")
+    
+}
+
+btnCloseMenu.onclick = function(){
+    resMenu.classList.toggle("none")
+}
